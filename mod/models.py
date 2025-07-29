1a326b7551e303f626c59c8e09f65f985d0f04b0
Patel-jainil-tft <patel.jainil@tftus.com>
2025-07-29 11:23:45 +0530

    Added date validator

--- mod/models.py
+++ mod/models.py
@@ -1,5 +1,7 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 from typing import Optional, Dict, Any
+from datetime import datetime
+
 class QueryRequest(BaseModel):
     message: str
     jobId: Optional[str] = None  # Changed to camelCase to match your input
@@ -25,6 +27,15 @@ class Intent(BaseModel):
     category: Optional[str]
     data: Dict[str, Any]
 
+    @field_validator("data")
+    def validate_applied_date(cls, v):
+        applied_date = v.get("appliedDate")
+        if applied_date:
+            try:
+                datetime.strptime(applied_date, "%d-%m-%Y")
+            except ValueError:
+                raise ValueError(f"appliedDate '{applied_date}' is not in DD-MM-YYYY format or not a real date.")
+        return v
 class AgentResponse(BaseModel):
     success: bool
     message: str
